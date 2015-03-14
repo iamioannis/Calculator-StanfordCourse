aa117ccd93e7bbf3dd351a035590a6e7879a31c3
ioannis <dev@iamioannis.com>
2015-03-14 19:59:51 +0000

    Lecture two exercise completed. Added: * Assigned functions to the buttons * Created collection to hold current calculations heap * Learned about case * Learned about closure // More practice needed * Learned about dynamic overriding of functions when similar ways to be called is expected increase simplicity but different functionality and design requirements apply, e.g. function performOperation() requires two Doubles but the calculation of square root only provides one * Learned how to apply constrains for auto layout to make UI work on multiple devices //More to come
    Extra:
    * Added clear button operation to clear the current heap and reset displayValue of the calculator to 0

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -24,6 +24,65 @@ class ViewController: UIViewController {
 			userIsInTheMiddleOfTypingANumber = true
 		}
 	}
-	//Blah Blah some more comments
+	
+	// Additional functionality. Not explained throught the lecture
+	// Double check if the way of reseting text is following best practice
+	@IBAction func clear() {
+		operandStack.removeAll(keepCapacity: false)
+		display.text = "0"
+	}
+	@IBAction func operate(sender: UIButton) {
+		let operation = sender.currentTitle!
+		if userIsInTheMiddleOfTypingANumber{
+			enter()
+		}
+		switch operation{
+		case "×": performOperation{ $0 * $1 }
+		case "÷": performOperation{ $1 / $0 }
+		case "+": performOperation{ $0 + $1 }
+		case "−": performOperation{ $1 - $0 }
+		case "√": performOperation{ sqrt($0) }
+		default: break
+		}
+		
+	}
+	
+	//Funcrion that performs AN operation taking TWO doubles as arguments and and returns a Double as the result
+	func performOperation(operation: (Double,Double) ->Double) {
+		if operandStack.count >= 2{
+			displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+			enter()
+		}
+
+	}
+	
+	// This is (if it can be described this way) an overide of the performOperation function for the purpose of handling single arguments during operation eg square root calculation.
+	func performOperation(operation: Double -> Double){
+		if operandStack.count >= 1{
+			displayValue = operation(operandStack.removeLast())
+			enter()
+		}
+		
+	}
+	
+	// Colection that stores values entered by the user. Can be preceaved as the heap for the calculations
+	var operandStack = Array<Double>()
+	
+	@IBAction func enter() {
+		userIsInTheMiddleOfTypingANumber = false
+		operandStack.append(displayValue)
+		println("operandStack = \(operandStack)")
+	}
+	
+	var displayValue: Double{
+		get{
+			return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+		}
+		set{
+			display.text = "\(newValue)"
+			userIsInTheMiddleOfTypingANumber = false
+		}
+	}
+	
 }
 
